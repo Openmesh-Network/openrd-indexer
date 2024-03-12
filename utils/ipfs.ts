f15b7c007e8d56e7520de72bbecd1a55bc2e0b52
Plopmenz <plopmenz@gmail.com>
2024-03-12 20:18:37 +0100

    ipfs get provider update

--- utils/ipfs.ts
+++ utils/ipfs.ts
@@ -49,8 +49,8 @@ export async function addToIpfs(json: string): Promise<string> {
 export async function getFromIpfs(hash: string): Promise<any> {
   let url: string;
   if (hash.startsWith("Qm")) {
-    // V0 IPFS hash
-    url = `https://ipfs.io/ipfs/${hash}`;
+    // V0 IPFS hash (will redirect to correct V1 hash on .ipfs.dweb.link)
+    url = `https://w3s.link/ipfs/${hash}`;
   } else {
     // V1 IPFS hash
     url = `https://${hash}.ipfs.w3s.link/`;
